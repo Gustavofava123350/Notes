99759f80a9b62fb07f4d88f7223b6672a630c0ed
Gustavofava123350 <gustavo_fava@outlook.com>
2024-12-02 09:55:37 -0300

    Project_laravel

--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -1,16 +1,9 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main_layout')
+@section('content')
    <h1>Welcome View and Blade ! </h1>
    <hr>
    <h3>Page2</h3>
    <hr>
    <h3>The value is: {{ $value }}</h3>
-</body>
-</html>
+@endsection
 
